bf52422450681bf7e3810da32e92343fc5c39cd8
michele <mbarrant@iu.edu>
2024-05-30 21:42:15 -0400

    finished scanner

--- PortScanner.py
+++ PortScanner.py
@@ -3,10 +3,17 @@
 import socket #the socket lib
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
+
+#host = "137.74.187.104"  #Ping the site you want IP address
+
+host = input("Enter IP to scan: ")
+port = int(input("Enter port to scan: "))
+
+#port = 22, should return false (ssh port)
 
 
-host = "137.74.187.104"  #Ping the site
-port = 443
+
 
 def portScanner(port):
     if s.connect_ex((host, port)):
@@ -14,3 +21,5 @@ def portScanner(port):
     else:
         print("port is open")
     
+
+portScanner(port)
